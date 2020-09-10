2e48307a9e0553fc041adeb73fb1deeede971007
Dan Nelson <dnelson4993@outlook.com>
2020-09-10 18:18:50 -0400

    Added weekly and daily views as well as added fake data to populate days with. Created logic to lop only over 3 events and when there are more give user the ability to show more and automatically go to the daily view

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-auto p-4 sm:p-6 lg:p-8">
     <div class="mx-auto">
-      <div class="bg-white overflow-hidden shadow rounded-lg">
+      <div class="bg-white shadow rounded-lg">
         <div class="bg-white px-4 py-5 border-b rounded-t-lg border-gray-200 sm:px-6">
           <div class="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-no-wrap">
             <div class="ml-4 mt-4">
@@ -19,8 +19,11 @@
                       </svg>
                     </button>
                   </div>
-                  <h3 class="text-xl leading-6 font-medium text-gray-900">
+                  <h3 v-if="currentCalendarLayout !== 'Day'" class="text-xl leading-6 font-medium text-gray-900">
                     {{ monthString }} {{ year }}
+                  </h3>
+                  <h3 v-else class="text-xl leading-6 font-medium text-gray-900">
+                    {{ monthString }} {{ currentDayOfMonth }}, {{ year }}
                   </h3>
                 </div>
               </div>
@@ -61,15 +64,83 @@
           </div>
         </div>
         <div>
-          <div class="grid grid-cols-7 gap-0">
-            <div :key="weekday" v-for="weekday in weekdaysAbv" class="p-0.5 flex justify-center">
-              <p class="text-xs uppercase font-semibold text-gray-500">{{ weekday }}</p>
+          <transition name="fade" mode="out-in">
+            <div :key="currentCalendarLayout" v-if="currentCalendarLayout === 'Month'" class="grid grid-cols-7 gap-0">
+              <div :key="weekday" v-for="weekday in weekdaysAbv" class="p-0.5 flex justify-center">
+                <p class="text-xs uppercase font-semibold text-gray-500">{{ weekday }}</p>
+              </div>
+              <div :key="n" v-for="n in 42" :class="determineBorder(n)" class="p-2 w-full border-gray-200" style="height: 10.8rem;">
+                <div class="text-center">
+                  <span v-if="(n - firstDayOfMonth) + 1 > 0 && (n - firstDayOfMonth) + 1 <= daysInMonth && !highlightCurrentDay((n - firstDayOfMonth) + 1)">
+                    <button @click="setCurrentDay((n - firstDayOfMonth) + 1)" class="px-3 py-0.5 rounded-full text-sm font-semibold leading-5 bg-white hover:bg-gray-200 transition ease-in-out duration-300 focus:outline-none">
+                      {{ (n - firstDayOfMonth) + 1 }}
+                    </button>
+                  </span>
+                  <span v-if="highlightCurrentDay((n - firstDayOfMonth) + 1)" class="inline-flex items-center">
+                    <button @click="setCurrentDay((n - firstDayOfMonth) + 1)" class="px-3 py-0.5 rounded-full text-sm font-semibold leading-5 text-white bg-gray-700 hover:bg-gray-300 transition ease-in-out duration-300 focus:outline-none">
+                      {{ (n - firstDayOfMonth) + 1 }}
+                    </button>
+                  </span>
+                </div>
+                <div class="mt-1.5">
+                  <ul class="grid grid-cols-1">
+                    <li :key="event.Title" v-for="event in mappedEvents((n - firstDayOfMonth) + 1)" class="mb-1.5 col-span-1 flex shadow-sm rounded-md">
+                      <div :class="determineEventColorClass(event.Type)" class="flex-shrink-0 flex items-center justify-center w-5 text-white text-sm leading-3 font-medium rounded-l-md">
+                        {{ event.Type[0] }}
+                      </div>
+                      <div class="pl-1 flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                        <div class="flex-1 text-sm leading-3 truncate">
+                          <a href="#" class="text-xs text-gray-900 font-medium hover:text-gray-600 transition ease-in-out duration-150">{{ event.Title }}</a>
+                          <p class="text-xs text-gray-500">{{ event.Time }}</p>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div v-if="numOfEventsByDay((n - firstDayOfMonth) + 1) > 3" class="mt-1">
+                  <button @click="setCurrentDay((n - firstDayOfMonth) + 1)" type="button" class="w-full px-2.5 py-1 border border-transparent text-xs leading-4 font-medium rounded text-gray-700 bg-gray-100 hover:bg-gray-50 focus:outline-none focus:border-gray-300 focus:shadow-outline-gray active:bg-gray-200 transition ease-in-out duration-150">
+                    View All {{ numOfEventsByDay((n - firstDayOfMonth) + 1) }} Events
+                  </button>
+                </div>
+              </div>
             </div>
-            <div :key="n" v-for="n in 42" :class="determineBorder(n)" class="p-2 h-24 w-full border-gray-200">
-              <span v-if="(n - (firstDayOfMonth)) + 1 > 0 && (n - (firstDayOfMonth)) + 1 <= daysInMonth">{{ (n - (firstDayOfMonth)) + 1 }}</span>
-              <!-- <p>{{ n }}</p> -->
+
+            <div :key="currentCalendarLayout" v-if="currentCalendarLayout === 'Week'" class="grid grid-cols-7 gap-0">
+              <div :key="weekday" v-for="(weekday, index) in weekdaysAbv" class="flex justify-center">
+                <span v-if="index !== currentDayOfWeek" class="p-1 text-xs uppercase font-semibold text-gray-500">{{ weekday }}</span>
+                <span v-if="index === currentDayOfWeek" class="p-1 text-xs uppercase font-semibold text-white bg-gray-600 w-full text-center">{{ weekday }}</span>
+              </div>
+              <div :key="n" v-for="n in 7" :class="determineBorder(n)" class="p-2 h-screen w-full border-gray-200">
+                <span>{{ (n + currentDayOfMonth) - currentDayOfWeek - 1 }}</span>
+              </div>
             </div>
-          </div>
+
+            <div :key="currentCalendarLayout" v-if="currentCalendarLayout === 'Day'" class="p-4">
+              <div>
+                <ul class="grid grid-cols-1 gap-5 sm:gap-6 sm:grid-cols-2">
+                  <li class="col-span-1 flex shadow-sm rounded-md" :key="event.Title" v-for="event in eventsForCurrentDay">
+                    <div :class="determineEventColorClass(event.Type)" class="flex-shrink-0 flex items-center justify-center w-16 text-white text-xs leading-5 font-semibold rounded-l-md">
+                      {{ event.Type }}
+                    </div>
+                    <div class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md truncate">
+                      <div class="flex-1 px-4 py-2 text-sm leading-5 truncate">
+                        <a href="#" class="text-gray-900 font-medium hover:text-gray-600 transition ease-in-out duration-150">{{ event.Title }}</a>
+                        <p class="text-gray-500">{{ event.Time }}</p>
+                      </div>
+                      <div class="flex-shrink-0 pr-2">
+                        <button class="w-8 h-8 inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition ease-in-out duration-150">
+                          <svg class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z" />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </transition>
+
         </div>
       </div>
     </div>
@@ -80,12 +151,24 @@
 export default {
   data() {
     return {
-      month: 1,
-      year: 2020,
+      month: new Date().getMonth() + 1,
+      year: new Date().getFullYear(),
+      currentDayOfMonth: new Date().getDate(),
+      currentDayOfWeek: new Date().getDay(),
       months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
       weekdaysAbv: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
       showCalendarTypeDropdown: false,
-      currentCalendarLayout: 'Month'
+      currentCalendarLayout: 'Month',
+      
+      calendarData: [
+        {'Type': 'Meeting', 'Title': 'Get coffee with Dave', 'DateTime':'September 20, 2020 12:00:00', 'Year': 2020, 'Month': 9, 'Day':10, 'Time': '12:00:00'},
+        {'Type': 'Workout', 'Title': 'Go for a bike ride', 'DateTime':'September 20, 2020 14:00:00', 'Year': 2020, 'Month': 9, 'Day':10, 'Time': '14:00:00'},
+        {'Type': 'Meeting', 'Title': 'Get coffee with Jane', 'DateTime':'September 20, 2020 12:00:00', 'Year': 2020, 'Month': 9, 'Day':20, 'Time': '12:00:00'},
+        {'Type': 'Workout', 'Title': 'Go for a bike ride', 'DateTime':'September 20, 2020 14:00:00', 'Year': 2020, 'Month': 9, 'Day':20, 'Time': '14:00:00'},
+        {'Type': 'Meeting', 'Title': 'Catch up with John', 'DateTime':'September 20, 2020 12:00:00', 'Year': 2020, 'Month': 9, 'Day':20, 'Time': '16:00:00'},
+        {'Type': 'Event', 'Title': 'Drinks for fundraiser', 'DateTime':'September 20, 2020 14:00:00', 'Year': 2020, 'Month': 9, 'Day':20, 'Time': '17:00:00'},
+        {'Type': 'Event', 'Title': 'Golf with Clients', 'DateTime':'September 20, 2020 14:00:00', 'Year': 2020, 'Month': 9, 'Day':20, 'Time': '17:00:00'}
+      ]
     }
   },
   computed: {
@@ -97,6 +180,21 @@ export default {
     },
     monthString: function() {
       return this.months[this.month - 1]
+    },
+    currentWeek: function() {
+      return Math.floor(this.currentDayOfMonth/7)
+    },
+    currentDate: function() {
+      const currentTimeStamp = new Date()
+      const dayOfMonth = currentTimeStamp.getDate()
+
+      return new Date(new Date().getFullYear(), new Date().getMonth(), dayOfMonth)
+    },
+    currentlySelectedDate: function() {
+      return new Date(this.year,this.month -1, this.currentDayOfMonth)
+    },
+    eventsForCurrentDay: function() {
+      return this.calendarData.filter(item => new Date(item.Year, item.Month - 1, item.Day).getTime() === this.currentlySelectedDate.getTime())
     }
   },
   methods: {
@@ -124,11 +222,42 @@ export default {
         this.month = 12
         this.year += -1
       }
+    },
+    setCurrentDay(day) {
+      const dateSelected = new Date(this.year, this.month -1, day)
+      this.currentDayOfMonth = day
+      this.currentDayOfWeek = dateSelected.getDay()
+      this.currentCalendarLayout = 'Day'
+    },
+    highlightCurrentDay(day){
+      const date = new Date(this.year, this.month -1, day)
+      return(date.getTime() === this.currentDate.getTime() ? true:false)
+    },
+    mappedEvents(day) {
+      const date = new Date(this.year, this.month -1, day)
+      return this.calendarData.filter(item => new Date(item.Year, item.Month - 1, item.Day).getTime() === date.getTime()).slice(0, 3)
+    },
+    numOfEventsByDay(day) {
+      const date = new Date(this.year, this.month -1, day)
+      return this.calendarData.filter(item => new Date(item.Year, item.Month - 1, item.Day).getTime() === date.getTime()).length
+    },
+    determineEventColorClass(eventType) {
+      const calendarEventColorMapping = [{'Type':'Meeting', 'Class':'bg-pink-600'}, {'Type':'Workout','Class':'bg-orange-500'}, {'Type':'Event','Class':'bg-green-400'}]
+      return(calendarEventColorMapping.find(type => type.Type === eventType).Class)
     }
   }
 }
 </script>
 
 <style>
-
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
 </style>
