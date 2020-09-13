9b5cb7b80cc69061d5e9e62b90e725fbfed0e51f
Dan Nelson <dan@Dans-MacBook-Pro.local>
2020-09-12 21:43:28 -0400

    Finished quick add event and added in form validation to ensure all fields are populated

--- pages/index.vue
+++ pages/index.vue
@@ -109,17 +109,53 @@
                                       Date
                                     </label>
                                     <div class="relative">
-                                      <h6 class="text-gray-600 text-sm leading-5">{{ $moment(currentlySelectedDate).format('MM/DD/YYYY') }}</h6>
+                                      <h6 class="mt-1 text-gray-600 text-sm leading-5">{{ $moment(currentlySelectedDate).format('MM/DD/YYYY') }}</h6>
                                     </div>
                                   </div>
                                   <div>
-                                    <label for="project_name" class="block text-sm font-medium leading-5 text-gray-900">
+                                    <label for="start_time" class="block text-sm font-medium leading-5 text-gray-900">
                                       Time
                                     </label>
                                     <div class="relative flex flex-no-wrap">
-                                      <input v-model="eventAdd_FormValues.Hour" :class="!validHourInput ? 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red':'focus:border-blue-300'" class="bg-white text-sm focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg px-2 block w-9 appearance-none leading-normal transition ease-in-out duration-150">
+                                      <!-- <input v-model="eventAdd_FormValues.Hour" :class="!validHourInput ? 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red':'focus:border-blue-300'" class="bg-white text-sm focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg px-2 block w-9 appearance-none leading-normal transition ease-in-out duration-150">
                                       <span class="block mx-1 font-black">:</span>
-                                      <input v-model="eventAdd_FormValues.Minute" :class="!validMinuteInput ? 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red':'focus:border-blue-300'" class="bg-white text-sm focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg px-2 block w-9 appearance-none leading-normal transition ease-in-out duration-150">
+                                      <input v-model="eventAdd_FormValues.Minute" :class="!validMinuteInput ? 'border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red':'focus:border-blue-300'" class="bg-white text-sm focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg px-2 block w-9 appearance-none leading-normal transition ease-in-out duration-150"> -->
+                                      <div class="px-2 py-1 bg-white text-sm focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg block appearance-none leading-normal transition ease-in-out duration-150">
+                                        <div class="flex">
+                                          <select v-model="eventAdd_FormValues.Hour" name="hours" class="text-sm bg-transparent appearance-none outline-none">
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                            <option value="6">6</option>
+                                            <option value="7">7</option>
+                                            <option value="8">8</option>
+                                            <option value="9">9</option>
+                                            <option value="10">10</option>
+                                            <option value="11">11</option>
+                                            <option value="12">12</option>
+                                            <option value="13">13</option>
+                                            <option value="14">14</option>
+                                            <option value="15">15</option>
+                                            <option value="16">16</option>
+                                            <option value="17">17</option>
+                                            <option value="18">18</option>
+                                            <option value="19">19</option>
+                                            <option value="20">20</option>
+                                            <option value="21">21</option>
+                                            <option value="22">22</option>
+                                            <option value="23">23</option>
+                                          </select>
+                                          <span class="text-sm font-black mx-2">:</span>
+                                          <select v-model="eventAdd_FormValues.Minute" name="minutes" class="text-sm bg-transparent appearance-none outline-none">
+                                            <option value="0">00</option>
+                                            <option value="15">15</option>
+                                            <option value="30">30</option>
+                                            <option value="45">45</option>
+                                          </select>
+                                        </div>
+                                      </div>
                                     </div>
                                   </div>
                                 </div>
@@ -156,7 +192,10 @@
                           </div>
                           <div class="flex justify-end mt-2">
                             <span class="inline-flex rounded-md shadow-sm">
-                              <button type="button" class="inline-flex items-center px-2 py-1 border border-transparent text-sm leading-6 font-medium rounded-md text-white bg-gray-600 hover:bg-gray-500 focus:outline-none focus:border-gray-700 focus:shadow-outline-gray active:bg-gray-700 transition ease-in-out duration-150">
+                              <button v-if="eventAddFormValidation" @click="addNewEvent((n - firstDayOfMonth) + 1)" type="button" class="inline-flex items-center px-2 py-1 border border-transparent text-sm leading-6 font-medium rounded-md text-white bg-gray-600 hover:bg-gray-500 focus:outline-none focus:border-gray-700 focus:shadow-outline-gray active:bg-gray-700 transition ease-in-out duration-150">
+                                Save
+                              </button>
+                              <button disabled v-else type="button" class="inline-flex items-center px-2 py-1 border border-transparent text-sm leading-6 font-medium rounded-md text-white bg-gray-400 cursor-default">
                                 Save
                               </button>
                             </span>
@@ -170,7 +209,7 @@
                   <div>
                     <span v-if="(n - firstDayOfMonth) + 1 > 0 && (n - firstDayOfMonth) + 1 <= daysInMonth && !highlightCurrentDay((n - firstDayOfMonth) + 1)">
                       <button @click="setCurrentDay((n - firstDayOfMonth) + 1), currentCalendarLayout = 'Day'" class="px-3 py-0.5 rounded-full text-sm font-semibold leading-5 bg-white hover:bg-gray-200 transition ease-in-out duration-300 focus:outline-none">
-                        {{ (n - firstDayOfMonth) + 1 }} |||| {{ n }}
+                        {{ (n - firstDayOfMonth) + 1 }}
                       </button>
                     </span>
                     <span v-if="highlightCurrentDay((n - firstDayOfMonth) + 1)" class="inline-flex items-center">
@@ -196,7 +235,7 @@
                   </ul>
                 </div>
                 <div v-if="numOfEventsByDay((n - firstDayOfMonth) + 1) > 3" class="mt-1">
-                  <button @click="setCurrentDay((n - firstDayOfMonth) + 1)" type="button" class="w-full px-2.5 py-1 border border-transparent text-xs leading-4 font-medium rounded text-gray-700 bg-gray-100 hover:bg-gray-50 focus:outline-none focus:border-gray-300 focus:shadow-outline-gray active:bg-gray-200 transition ease-in-out duration-150">
+                  <button @click="setCurrentDay((n - firstDayOfMonth) + 1), currentCalendarLayout = 'Day'" type="button" class="w-full px-2.5 py-1 border border-transparent text-xs leading-4 font-medium rounded text-gray-700 bg-gray-100 hover:bg-gray-50 focus:outline-none focus:border-gray-300 focus:shadow-outline-gray active:bg-gray-200 transition ease-in-out duration-150">
                     View All ({{ numOfEventsByDay((n - firstDayOfMonth) + 1) }})
                   </button>
                 </div>
@@ -323,7 +362,7 @@ export default {
     },
     validHourInput: function() {
       if(this.eventAdd_FormValues.Hour) {
-          const re = /0[1-9]|1[0-2]|[1-9]/;
+          const re = /0?[1-9]|1[0-2]/;
           return re.test(String(this.eventAdd_FormValues.Hour));
       } else {
           return true
@@ -336,6 +375,13 @@ export default {
       } else {
           return true
       }
+    },
+    eventAddFormValidation: function() {
+      if(this.eventAdd_FormValues.Hour && this.eventAdd_FormValues.Minute && this.eventAdd_FormValues.Type && this.eventAdd_FormValues.Title && this.eventAdd_FormValues.Description) {
+        return true
+      } else {
+        return false
+      }
     }
   },
   watch: {
@@ -346,6 +392,22 @@ export default {
     }
   },
   methods: {
+    addNewEvent(day) {
+      const eventDate = new Date(this.year, this.month-1, day, this.eventAdd_FormValues.Hour, this.eventAdd_FormValues.Minute)
+      const eventObject = {
+                            'Type': this.eventAdd_FormValues.Type, 
+                            'Title': this.eventAdd_FormValues.Title, 
+                            'Description': this.eventAdd_FormValues.Description,
+                            'DateTime': eventDate, 
+                            'Year': this.year, 
+                            'Month': this.month, 
+                            'Day':day, 
+                            'Time': this.eventAdd_FormValues.Hour + ':' + this.eventAdd_FormValues.Minute + ':00'
+                          }
+
+      this.calendarData.push(eventObject)
+      this.resetQuickAddForm()
+    },
     determineBorder(day) {
       const borderLeftDays = [1,8,15,22,29,36]
 
@@ -406,6 +468,17 @@ export default {
       } else {
         return 'left-0'
       }
+    },
+    resetQuickAddForm() {
+      this.eventAdd_FormValues.Date = null
+      this.eventAdd_FormValues.Time = null
+      this.eventAdd_FormValues.Hour = null
+      this.eventAdd_FormValues.Minute = null
+      this.eventAdd_FormValues.Type = null
+      this.eventAdd_FormValues.Title = null
+      this.eventAdd_FormValues.Description = null
+
+      this.showQuickAddDropDown = false
     }
   }
 }
