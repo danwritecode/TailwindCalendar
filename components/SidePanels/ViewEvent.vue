00f0c95abd8f8c1facf6af9a228f11e1d50fadff
Dan Nelson <dnelson4993@outlook.com>
2020-09-16 16:29:38 -0400

    integrated edit form into view event component

--- components/SidePanels/ViewEvent.vue
+++ components/SidePanels/ViewEvent.vue
@@ -38,34 +38,111 @@
                     </div>
                   </div>
                 </header>
-                <div class="px-4 py-5">
-                    <dl>
-                      <div class="mt-4">
-                        <dt class="text-sm leading-5 font-medium text-gray-500">
-                          When
-                        </dt>
-                        <dd class="text-sm leading-5 text-gray-900">
-                          {{ $moment(event.DateTime).format('lll') }}
-                        </dd>
+                <div v-if="formState === 'View'" class="px-4 py-5">
+                  <dl>
+                    <div class="mt-4">
+                      <dt class="text-sm leading-5 font-medium text-gray-500">
+                        When
+                      </dt>
+                      <dd class="text-sm leading-5 text-gray-900">
+                        {{ $moment(event.DateTime).format('lll') }}
+                      </dd>
+                    </div>
+                    <div class="mt-4">
+                      <dt class="text-sm leading-5 font-medium text-gray-500">
+                        Title
+                      </dt>
+                      <dd class="text-sm leading-5 text-gray-900">
+                        {{ event.Title }}
+                      </dd>
+                    </div>
+                    <div class="mt-4">
+                      <dt class="text-sm leading-5 font-medium text-gray-500">
+                        Body
+                      </dt>
+                      <dd class="text-sm leading-5 text-gray-900">
+                        {{ event.Body }}
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+                <div v-if="formState === 'Edit'" class="flex-1 flex flex-col justify-between">
+                  <div class="px-4 divide-y divide-gray-200 sm:px-6">
+                    <div class="space-y-6 pt-6 pb-5">
+                      <div>
+                        <label for="start_time" class="block text-sm font-medium leading-5 text-gray-900">
+                          Time
+                        </label>
+                        <div class="relative flex flex-no-wrap">
+                          <div class="px-2 py-1 bg-white text-sm focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg block appearance-none leading-normal transition ease-in-out duration-150">
+                            <div class="flex">
+                              <select v-model="dateTimeHour" name="hours" class="text-sm bg-transparent appearance-none outline-none">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                                <option value="22">22</option>
+                                <option value="23">23</option>
+                              </select>
+                              <span class="text-sm font-black mx-2">:</span>
+                              <select v-model="dateTimeMinute" name="minutes" class="text-sm bg-transparent appearance-none outline-none">
+                                <option value="0">00</option>
+                                <option value="15">15</option>
+                                <option value="30">30</option>
+                                <option value="45">45</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                      <div class="mt-4">
-                        <dt class="text-sm leading-5 font-medium text-gray-500">
+                      <div class="space-y-1">
+                        <label for="event_type" class="block text-sm font-medium leading-5 text-gray-900">
+                          Type
+                        </label>
+                        <div class="relative rounded-md shadow-sm">
+                          <select v-model="event.Type" id="location" class="mt-1 form-select block w-full pl-3 pr-10 py-2 text-base leading-6 border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5">
+                            <option>Meeting</option>
+                            <option>Event</option>
+                            <option>Workout</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="space-y-1">
+                        <label for="project_name" class="block text-sm font-medium leading-5 text-gray-900">
                           Title
-                        </dt>
-                        <dd class="text-sm leading-5 text-gray-900">
-                          {{ event.Title }}
-                        </dd>
+                        </label>
+                        <div class="relative rounded-md shadow-sm">
+                          <input v-model="event.Title" id="event_title" class="form-input block w-full sm:text-sm sm:leading-5 transition ease-in-out duration-150">
+                        </div>
                       </div>
-                      <div class="mt-4">
-                        <dt class="text-sm leading-5 font-medium text-gray-500">
+                      <div class="space-y-1">
+                        <label for="project_name" class="block text-sm font-medium leading-5 text-gray-900">
                           Body
-                        </dt>
-                        <dd class="text-sm leading-5 text-gray-900">
-                          {{ event.Body }}
-                        </dd>
+                        </label>
+                        <div class="relative rounded-md shadow-sm">
+                          <input v-model="event.Body" id="event_body" class="form-input block w-full sm:text-sm sm:leading-5 transition ease-in-out duration-150">
+                        </div>
                       </div>
-                    </dl>
+                    </div>
                   </div>
+                </div>
               </div>
               <div class="flex-shrink-0 px-4 py-4 space-x-4 flex justify-end">
                 <span class="inline-flex rounded-md shadow-sm">
@@ -74,8 +151,11 @@
                   </button>
                 </span>
                 <span class="inline-flex rounded-md shadow-sm">
-                  <button type="submit" :class="determineEventColorClass(event.Type)"  class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white focus:outline-none transition duration-150 ease-in-out">
+                  <button v-if="formState === 'View'" @click="formState = 'Edit'" type="submit" :class="determineEventColorClass(event.Type)"  class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white focus:outline-none transition duration-150 ease-in-out">
                     Edit
+                  </button>
+                  <button v-if="formState === 'Edit'" @click="formState = 'View'" type="submit" :class="determineEventColorClass(event.Type)"  class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white focus:outline-none transition duration-150 ease-in-out">
+                    Save
                   </button>
                 </span>
               </div>
@@ -92,7 +172,16 @@ export default {
   data() {
     return {
       event: null,
-      animationToggle: false
+      animationToggle: false,
+      formState: 'View'
+    }
+  },
+  computed: {
+    dateTimeHour: function() {
+      return new Date(this.event.DateTime).getHours()
+    },
+    dateTimeMinute: function() {
+      return new Date(this.event.DateTime).getMinutes()
     }
   },
   created() {
