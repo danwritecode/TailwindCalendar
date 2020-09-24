35ba3bdc2ccb59c14ceda3b58cba34a8f9ba7d43
Dan Nelson <dnelson4993@outlook.com>
2020-09-24 11:16:14 -0400

    added highlight of current date on yearly view and also changed each month in yearly view to a button and added hovering effects to show which month the user is hovering

--- components/Calendar/YearlyView.vue
+++ components/Calendar/YearlyView.vue
@@ -3,16 +3,16 @@
     <div :key="year" v-for="year in 15" class="mb-2 w-full pb-8">
       <h3 class="leading-5 font-medium text-xl text-gray-800 border-b border-gray-200 pb-1">{{ startYear + (year-1) }} </h3>
       <div class="mt-3 grid grid-cols-3 gap-1.5">
-        <div :key="month" v-for="month in 12" class="w-full" style="height:6.5rem;">
-          <h6 class="px-1 leading-5 text-xs font-medium text-gray-600">{{ months[month-1] }}</h6>
+        <button type="button" :key="month" v-for="month in 12" class="w-full text-gray-600 hover:text-black hover:font-medium hover:bg-gray-100 rounded-md transition ease-in-out duration-150" style="height:6.5rem;">
+          <h6 class="text-left px-1 leading-5 text-xs font-medium">{{ months[month-1] }}</h6>
           <div class="-mt-2 grid grid-cols-7" style="grid-gap: 0.10rem; gap: 0.10rem;">
             <div :key="day" v-for="day in 37" class="text-center h-3">
-              <span class=" leading-none" style="font-size:0.51rem" v-if="((day - firstDayOfMonth(startYear + (year-1), month)) + 1) > 0 && ((day - firstDayOfMonth(startYear + (year-1), month)) + 1) <= daysInMonth(startYear + (year-1), month)">
+              <span :class="highlightCurrentDay((day - firstDayOfMonth(startYear + (year-1), month)) + 1, month, startYear + (year-1)) ? 'text-red-500 font-black border border-red-500 rounded-full p-0.5':'text-black'" class="leading-none" style="font-size:0.51rem" v-if="((day - firstDayOfMonth(startYear + (year-1), month)) + 1) > 0 && ((day - firstDayOfMonth(startYear + (year-1), month)) + 1) <= daysInMonth(startYear + (year-1), month)">
                 {{ (day - firstDayOfMonth(startYear + (year-1), month)) + 1 }}
               </span>
             </div>
           </div>
-        </div>
+        </button>
       </div>
     </div>
   </div>
@@ -26,13 +26,25 @@ export default {
       months: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
     };
   },
-  methods: {
-    firstDayOfMonth(year, month) {
-      return new Date(year, month-1, 1).getDay() + 1
+  computed: {
+    currentDate: function() {
+      const currentTimeStamp = new Date()
+      const dayOfMonth = currentTimeStamp.getDate()
+
+      return new Date(new Date().getFullYear(), new Date().getMonth(), dayOfMonth)
     },
+  },
+  methods: {
     daysInMonth(year, month) {
       return new Date(year, month, 0).getDate()
     },
+    firstDayOfMonth(year, month) {
+      return new Date(year, month-1, 1).getDay() + 1
+    },
+    highlightCurrentDay(day, month, year){
+      const date = new Date(year, month-1, day)
+      return(date.getTime() === this.currentDate.getTime() ? true:false)
+    }
   }
 };
 </script>
